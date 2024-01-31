126edf61df7fa079429b06d7c033fd310e893de9
kalai0304 <kalaiyarasan0304@gmail.com>
2024-02-01 01:04:39 +0530

    source files added

---
+++ script_todolist.js
@@ -0,0 +1,26 @@
+let item = document.getElementById("box");
+item.addEventListener("keypress",function(event){
+    if(event.key === "Enter"){
+        event.preventDefault();
+        document.getElementById("btn").click();
+    }
+    
+});
+
+function add_item(){
+    // let item = document.getElementById("box");
+    let list_item=document.getElementById("list_item");
+    if(item.value != ""){
+        let make_li=document.createElement("li");
+        make_li.appendChild(document.createTextNode(item.value));
+        list_item.appendChild(make_li);
+        item.value="";
+
+        make_li.onclick = function(){
+            this.parentNode.removeChild(this);
+        };
+    }
+    else{
+        alert("Please add a value to list");
+    }
+}
